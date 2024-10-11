ad03d60348359633e89cf4bdb58c131fd7e5f0c2
Abdalla <a.eltayeb@campus.tu-berlin.de>
2024-10-11 10:19:28 +0200

    add similarity score

--- testing/quality_assesment.py
+++ testing/quality_assesment.py
@@ -1,11 +1,12 @@
 from datasets import Dataset 
-from ragas.metrics import FaithulnesswithHHEM
 from ragas.metrics import faithfulness
 from ragas import evaluate
 import os
 import pandas as pd
 from dotenv import load_dotenv
 import ast
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 load_dotenv()
 
@@ -14,9 +15,21 @@ os.environ["LANGCHAIN_ENDPOINT"] = os.environ.get("LANGCHAIN_ENDPOINT")
 os.environ["LANGCHAIN_API_KEY"] = os.environ.get("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_PROJECT"] = os.environ.get("LANGCHAIN_PROJECT")
 
-NUM_OF_STATEMENTS = 100
+NUM_OF_STATEMENTS = 5
 
-faithfulness_with_hhem = FaithulnesswithHHEM()
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v2')
+
+
+def compute_similarity(source_sentence, comparison_sentences):
+    # Encode the source sentence and comparison sentences
+    source_embedding = model.encode([source_sentence])
+    comparison_embeddings = model.encode(comparison_sentences)
+
+    # Compute cosine similarity between the source sentence and each comparison sentence
+    similarities = cosine_similarity(source_embedding, comparison_embeddings)
+
+    # Return the similarity scores (it's a 1xN matrix, so we extract the row)
+    return similarities[0]
 
 def clean_context(context):
     return [info[0] for _, infos in context for info in infos]
@@ -25,7 +38,7 @@ def preprocess(strategy, model):
     file_path = f'results_new_dataset/{model}/{strategy}_{model}.xlsx'
     sampled_data = pd.read_excel(file_path)
     sampled_data = sampled_data.head(NUM_OF_STATEMENTS)
-    sampled_data['Statement'] = sampled_data.apply(lambda row: f'Is this statement made by {row["Name"]} true: {row["Statement"]}', axis=1)
+    sampled_data['Formatted Statement'] = sampled_data.apply(lambda row: f'Is this statement made by {row["Name"]} true: {row["Statement"]}', axis=1)
     sampled_data['Retrieved Information'] = sampled_data['Retrieved Information'].apply(lambda context: clean_context(ast.literal_eval(context)))
     return sampled_data
 
@@ -37,36 +50,40 @@ def evaluate_strategies(strategy, model):
     if os.path.exists(output_file_path):
         evaluated_data = pd.read_excel(output_file_path)
     else:
-        evaluated_data = pd.DataFrame(columns=['Statement', 'Name', 'Explanation', 'Retrieved Information', 'Faithfulness'])
+        evaluated_data = pd.DataFrame(columns=['Statement', 'Formatted Statement', 'Name', 'Explanation', 'Retrieved Information', 'Faithfulness', 'Sentence Similarity'])
 
     
     for index, row in sampled_data.iterrows():
         # Check if the current statement has already been processed (to avoid duplicates)
-        if not evaluated_data[evaluated_data['Statement'] == row['Statement']].empty:
-            print(f"Statement {row['Statement']} already processed. Skipping.")
+        if not evaluated_data[evaluated_data['Formatted Statement'] == row['Formatted Statement']].empty:
+            print(f"Statement {row['Formatted Statement']} already processed. Skipping.")
             continue
-        
+     
         data_samples = {
-            'question': [row['Statement']],
+            'question': [row['Formatted Statement']],
             'answer': [row['Explanation']],
             'contexts' : [row['Retrieved Information']],
         }
         
         dataset = Dataset.from_dict(data_samples)       
         faithfulness_score = 0
+        sentence_similarity = 0
         if row['Retrieved Information']:
             score = evaluate(dataset,metrics=[faithfulness])
             res_df = score.to_pandas()
             faithfulness_score = res_df['faithfulness']
+            sentence_similarity = compute_similarity(row['Statement'], row['Retrieved Information'])
         else:
             print("No retrieved information!")
         
         temp_df = pd.DataFrame({
+            'Formatted Statement': [row['Formatted Statement']],
             'Statement': [row['Statement']],
             'Name': [row['Name']],
             'Explanation': [row['Explanation']],
             'Retrieved Information': [row['Retrieved Information']],
-            'Faithfulness': faithfulness_score
+            'Faithfulness': faithfulness_score,
+            'Sentence Similarity': sentence_similarity[0],
         })
         
         evaluated_data = pd.concat([evaluated_data, temp_df], ignore_index=True)
